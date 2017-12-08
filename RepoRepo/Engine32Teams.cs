c71cd0c4ba97105bab7a35614d67f2fd9c9e79c8
laamimer <laamimer@gmail.com>
2017-12-08 10:36:18 +0000

    Revert "absolutePath"
    This reverts commit 938db72c0ac54e7eaa41ffdbd9e19f3397ab98e1.

--- RepoRepo/Engine32Teams.cs
+++ RepoRepo/Engine32Teams.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Windows.Forms;
 
 namespace RepoRepo
 {
     public class Engine32Teams
     {
-
-        
-
         List<Point> positionList = new List<Point>();
 
     }
--- RepoRepo/Form1.cs
+++ RepoRepo/Form1.cs
@@ -16,38 +16,18 @@ namespace RepoRepo
         public Form1()
         {
             InitializeComponent();
-            Image menu = Image.FromFile(@"..\..\Sprites\main\menu1.png");
+            Image menu = Image.FromFile(@"C:\Users\Amine\Desktop\WoldCup2018\Sprites\main\menu1.png");
             pictureBox1.Size = new Size(menu.Width, menu.Height); //1150 x 715
             Size = new Size(menu.Width + 400, menu.Height);
-            StartPosition = FormStartPosition.CenterScreen;
             pictureBox1.Left = 200;
             pictureBox1.Top = 0;
             pictureBox1.Image = menu;
                     
             Team team1 = new Team();
-            this.Controls.Add(team1.SetFlag(@"..\..\Sprites\teams\argentina.png"));
+            this.Controls.Add(team1.SetFlag(@"C:\Users\Amine\Desktop\WoldCup2018\Sprites\teams\argentina.png"));
             team1.Show();
 
         }
-
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
-        {
-            Point buttonPosition = new Point(0, 0);
-
-            debugTextBox.Text = e.Location.ToString();
-            
-        }
-
-        private Point GetPosition(Control c)
-        {
-            return c.FindForm().PointToClient(c.Parent.PointToScreen(c.Location));
-        }
-
-        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
-        {
-            debugTextBox.Text = e.Location.ToString();
-        }
-
         /*
         private void button1_Click(object sender, EventArgs e)
         {
